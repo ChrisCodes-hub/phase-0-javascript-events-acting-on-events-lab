78194ab8b7fce7788442f049d19bf8b0f7e3e55d
christopherchege <chrisshege35@gmail.com>
2025-03-22 02:14:05 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft(){
+        const leftNumbers =dodger.style.left.replace('px','');
+        const left = parseInt(leftNumbers,10);
+        dodger.style.left =`${left -10}px`
+    };
+  
+
+
+  function moveDodgerRight(){
+        const rightNum =dodger.style.left.replace('px','');
+        const right = parseInt(rightNum,10);
+        dodger.style.left =`${right +10}px`
+    };
+
+    
